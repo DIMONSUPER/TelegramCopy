bf503afda6bea3befa700a9ff9ed55b672dbec6a
Dmytro Fedchenko <fedchenko@dlit.dp.ua>
2023-07-27 16:32:46 +0300

    git init

---
+++ TelegramCopy/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TelegramCopy;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ TelegramCopy/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TelegramCopy;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
