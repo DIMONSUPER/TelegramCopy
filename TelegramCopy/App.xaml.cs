32fa4b5b3735a7597078597f9e1da40ae110604f
Dmytro Fedchenko <fedchenko@dlit.dp.ua>
2023-07-28 11:50:28 +0300

    - Minimum window size was implemented

--- TelegramCopy/App.xaml.cs
+++ TelegramCopy/App.xaml.cs
@@ -6,5 +6,19 @@ public partial class App : Application
 	{
 		InitializeComponent();
 	}
+
+    #region -- Overrides --
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        window.MinimumWidth = Constants.MINIMUM_WINDOW_WIDTH;
+        window.MinimumHeight = Constants.MINIMUM_WINDOW_HEIGHT;
+
+        return window;
+    }
+
+    #endregion
 }
 
--- TelegramCopy/Constants.cs
+++ TelegramCopy/Constants.cs
@@ -3,5 +3,9 @@
 public static class Constants
 {
     public const string DATABASE_NAME = "TelegramCopy.db3";
+    public const int CURRENT_USER_ID = 1;
+
+    public const int MINIMUM_WINDOW_WIDTH = 680;
+    public const int MINIMUM_WINDOW_HEIGHT = 720;
 }
 
